79bd40c060f7043b703fd8e17d4aadefd4db74ee
Franz Mamani <franzmamanich1991@gmail.com>
2021-04-15 23:41:43 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,16 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class AppComponent {
   title = 'YoutubeNG';
-  video: any = null;
-  /**
-   *
-   */
-  constructor(private sanitizer: DomSanitizer) {
-    this.getVideoIframe('https://www.youtube.com/watch?v=8ILiiDpa4tk&list=PLV0nOzdUS5Xsj5o9c7HgLqYt7EV9MPOqD');
-  }
-  getVideoIframe(url: any) {
-      var results: any = null;
-      if (url === null) {
-          return '';
-      }
-      results = url.match('[\\?&]v=([^&#]*)');
-      this.video   = (results === null) ? url : results[1];
-      return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.video);
+  constructor(private sanitizer: DomSanitizer) {}
+  getVideoIframe(url) {
+    // tslint:disable-next-line:one-variable-per-declaration
+    var video, results;
+    if (url === null) {
+        return '';
+    }
+    results = url.match('[\\?&]v=([^&#]*)');
+    video   = (results === null) ? url : results[1];
+ 
+    return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);   
   }
 }
